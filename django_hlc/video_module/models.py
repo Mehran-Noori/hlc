0bd17d3435470e49ee8d7a192be713ab9a0134c5
Mehran_Noori <mehrannori54@gmail.com>
2024-03-07 18:24:21 +0330

    fix: fix video model

--- django_hlc/video_module/models.py
+++ django_hlc/video_module/models.py
@@ -4,8 +4,8 @@ from django.utils.text import slugify
 
 class Video(models.Model):
     title = models.CharField(
-        max_length=150, verbose_name="video title", null=False, blank=False, required=True)
-    slug = models.SlugField(_("video_slug"))
+        max_length=150, verbose_name="video title", null=False, blank=False)
+    slug = models.SlugField(verbose_name="video title")
     file = models.FileField(upload_to="files/videos", null=False)
     created_at = models.DateTimeField(
         verbose_name='created at', auto_now_add=True, editable=False, null=True)
@@ -13,9 +13,8 @@ class Video(models.Model):
         verbose_name='last update', auto_now=True, null=True)
 
     class Meta:
-        verbose_name = _("Video")
-        verbose_name_plural = _("Videos")
-
+        verbose_name = "Video"
+        verbose_name_plural = "Videos"
     def __str__(self):
         return self.name
     
